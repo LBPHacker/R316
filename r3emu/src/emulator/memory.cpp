41f23206afc8876e6eec2a7e251c96a25cceac32
Tamás Bálint Misius <lbphacker@gmail.com>
2019-06-12 14:04:27 +0200

    Add bound checking to the Lua interface of main memory

--- r3emu/src/emulator/memory.cpp
+++ r3emu/src/emulator/memory.cpp
@@ -3,6 +3,9 @@
 #include "../config.hpp"
 #include "../lua/state.hpp"
 
+#include <iostream>
+#include <iomanip>
+
 namespace r3emu::emulator
 {
 	memory::memory(lua::state &L_param, std::string name_param) :
@@ -16,15 +19,30 @@ namespace r3emu::emulator
 			auto *mem = static_cast<memory *>(lua_touserdata(L, lua_upvalueindex(1)));
 			uint16_t addr = luaL_checkinteger(L, 1);
 			uint32_t value = luaL_checkinteger(L, 2);
-			mem->data[addr] = value;
+			if (addr < (1 << config::memory_size))
+			{
+				mem->data[addr] = value;
+			}
+			else
+			{
+				luaL_error(L, "out-of-bounds write: data[0x%04X] = 0x%08X", addr, value);
+			}
 			return 0;
 		}, "write");
 
 		L.set_ugly_func(this, [](lua_State *L) -> int {
 			auto *mem = static_cast<memory *>(lua_touserdata(L, lua_upvalueindex(1)));
 			uint16_t addr = luaL_checkinteger(L, 1);
-			lua_pushinteger(L, mem->data[addr]);
-			return 1;
+			if (addr < (1 << config::memory_size))
+			{
+				lua_pushinteger(L, mem->data[addr]);
+				return 1;
+			}
+			else
+			{
+				luaL_error(L, "out-of-bounds read: return data[0x%04X]", addr);
+			}
+			return 0;
 		}, "read");
 
 		lua_setglobal(L, name.c_str());
